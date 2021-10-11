e50f1cff31894ac909945ea2843bee433e10f388
nghiatran98 <nghiatran19980613@gmail.com>
2021-10-11 13:50:24 +0700

    main scene
    - added Animal script
    - added GameManager script
    - added PlayerController script

--- Programming Theory Project/Assets/Scripts/Animal.cs
+++ Programming Theory Project/Assets/Scripts/Animal.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
+
+    public float speed = 2021;
+
     private Rigidbody animalRb;
-    public float speed = 500;
+    private Vector3 com;
+
+    private float topBound = 50;
+    private float bottomBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
         animalRb = GetComponent<Rigidbody>();
+        animalRb.centerOfMass = com;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        moveDown();
+        MoveDown();
+        BoundScope();
     }
 
-    protected void moveDown()
+    protected void MoveDown()
     {
-        animalRb.AddForce(Vector3.forward * speed);
+
+        animalRb.AddRelativeForce(Vector3.forward * speed);
     }
+
+    void BoundScope()
+    {
+        float animalPosZ = transform.position.z;
+
+        if (animalPosZ < bottomBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
